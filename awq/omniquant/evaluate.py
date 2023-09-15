3c1c7bffd9eba67a7037022dde7c9334ed1a916a
Alexander Prutko <alexander.prutko@gmail.com>
2023-09-15 07:16:31 +0000

    eval files from omniquant

---
+++ awq/omniquant/evaluate.py
@@ -0,0 +1,113 @@
+import os
+import torch
+from torch import nn
+import numpy as np
+from lm_eval import evaluator
+from tqdm import tqdm
+from pprint import pprint
+
+from datautils import get_loaders
+from categories import subcategories, categories
+
+
+@torch.no_grad()
+def evaluate(lm, args, logger):
+    results = {}
+    if "opt" in args.net.lower():
+        lm.model.model.decoder = lm.model.model.decoder.to(lm.device)
+    elif "llama" in args.net.lower():
+        lm.model = lm.model.to(lm.device)
+    elif "falcon" in args.net.lower():
+        lm.model.transformer = lm.model.transformer.to(lm.device)
+
+    if args.eval_ppl:
+        for dataset in ["wikitext2", "ptb", "c4","ptb-new",'c4-new']:
+            cache_testloader = f'{args.cache_dir}/testloader_{args.model_family}_{dataset}_all.cache'
+            if os.path.exists(cache_testloader):
+                testloader = torch.load(cache_testloader)
+                logger.info(f"load calibration from {cache_testloader}")
+            else:
+                dataloader, testloader = get_loaders(
+                    dataset,
+                    seed=args.seed,
+                    model=args.model,
+                    seqlen=lm.seqlen,
+                )
+                torch.save(testloader, cache_testloader)
+            if "c4" in dataset:
+                testenc = testloader
+            else:
+                testenc = testloader.input_ids
+
+            nsamples = testenc.numel() // lm.seqlen
+            use_cache = lm.model.config.use_cache
+            lm.model.config.use_cache = False
+            lm.model.eval()
+            nlls = []
+            for i in tqdm(range(nsamples)):
+                batch = testenc[:, (i * lm.seqlen) : ((i + 1) * lm.seqlen)].to(lm.device)
+                if "opt" in args.net.lower():
+                    outputs = lm.model.model.decoder(batch)
+                elif "llama" in args.net.lower():
+                    outputs = lm.model.model(batch)
+                elif "falcon" in args.model:
+                    outputs = lm.model.transformer(batch)
+                hidden_states = outputs[0]
+                logits = lm.model.lm_head(hidden_states)
+                shift_logits = logits[:, :-1, :]
+                shift_labels = testenc[:, (i * lm.seqlen) : ((i + 1) * lm.seqlen)][
+                    :, 1:
+                ].to(lm.model.lm_head.weight.device)
+                loss_fct = nn.CrossEntropyLoss()
+                loss = loss_fct(
+                    shift_logits.view(-1, shift_logits.size(-1)),
+                    shift_labels.view(-1),
+                )
+                neg_log_likelihood = loss.float() * lm.seqlen
+                nlls.append(neg_log_likelihood)
+                if i == args.limit:
+                    break
+
+            ppl = torch.exp(torch.stack(nlls).sum() / (nsamples * lm.seqlen))
+            logger.info(f'{dataset} : {ppl.item()}')
+            lm.model.config.use_cache = use_cache
+            results[dataset] = ppl.item()
+    if args.tasks != "":
+        t_results = evaluator.simple_evaluate(
+            lm,
+            tasks=args.tasks,
+            num_fewshot=args.num_fewshot,
+            limit=None if args.limit == -1 else args.limit,
+        )
+        results.update(t_results)
+        logger.info(results)
+        pprint(results)
+        # for test of MMLU
+        if 'hendrycksTest' in args.tasks:
+            all_cors = []
+            all_cors_norm = []
+            subcat_cors = {subcat: [] for subcat_lists in subcategories.values() for subcat in subcat_lists}
+            cat_cors = {cat: [] for cat in categories}
+            cat_cors_norm = {cat: [] for cat in categories}
+            for key in t_results['results'].keys():
+                if not 'hendrycksTest' in key:
+                    continue
+                subject = key.split('-')[-1]
+                cors = t_results['results'][key]['acc']
+                cors_norm = t_results['results'][key]['acc_norm']
+                subcats = subcategories[subject]
+                for subcat in subcats:
+                    subcat_cors[subcat].append(cors)
+                    for key in categories.keys():
+                        if subcat in categories[key]:
+                            cat_cors[key].append(cors)
+                            cat_cors_norm[key].append(cors_norm)
+                    all_cors.append(cors)
+                    all_cors_norm.append(cors_norm)
+                    
+            for cat in cat_cors:
+                cat_acc = np.mean(cat_cors[cat])
+                logger.info("Average accuracy {:.4f} - {}".format(cat_acc, cat))
+            weighted_acc = np.mean(all_cors)
+            logger.info("Average accuracy: {:.4f}".format(weighted_acc))               
+    return results
